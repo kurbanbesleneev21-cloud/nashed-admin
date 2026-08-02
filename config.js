a34b6e8e16ea99f11e0020760f72a155fa1677e6
kurbanbesleneev21-cloud <kurbanbesleneev21@gmail.com>
2026-08-02 19:06:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const CONFIG = {
     // GitHub настройки
     GITHUB_USERNAME: 'kurbanbesleneev21-cloud',
     GITHUB_REPO: 'Nashed',
-    GITHUB_TOKEN: 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 / 
+    GITHUB_TOKEN: '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' / 
     
     // Путь к аудио файлам в репозитории
     AUDIO_PATH: 'audio'
