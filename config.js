fa9a4582f56d22c68a25f8795dc6e2e3e25910e4
kurbanbesleneev21-cloud <kurbanbesleneev21@gmail.com>
2026-08-02 19:29:47 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const CONFIG = {
     ADMIN_PASSWORD: 'admin123',
     GITHUB_USERNAME: 'kurbanbesleneev21-cloud',
     GITHUB_REPO: 'Nashed',
-    GITHUB_TOKEN: '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',
-    AUDIO_PATH: 'audio'
+    GITHUB_TOKEN: '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'
+    ,AUDIO_PATH: 'audio'
 };
