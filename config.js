449a96d39afa3fbaea9fd534b4743c2312916d78
kurbanbesleneev21-cloud <kurbanbesleneev21@gmail.com>
2026-08-02 19:18:36 +0300

    Change GITHUB_TOKEN in config.js
    Updated GITHUB_TOKEN to a new value.

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const CONFIG = {
     ADMIN_PASSWORD: 'admin123',
     GITHUB_USERNAME: 'kurbanbesleneev21-cloud',
     GITHUB_REPO: 'Nashed',
-    GITHUB_TOKEN: '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',
+    GITHUB_TOKEN: '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',
     AUDIO_PATH: 'audio'
 };
