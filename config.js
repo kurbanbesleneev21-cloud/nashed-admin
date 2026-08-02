afbe1e1129e676399eb14c22eedcc248caad92e5
kurbanbesleneev21-cloud <kurbanbesleneev21@gmail.com>
2026-08-02 18:35:36 +0300

    Change GitHub token in config.js
    Updated GitHub token in config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const CONFIG = {
     // GitHub настройки
     GITHUB_USERNAME: 'kurbanbesleneev21-cloud',
     GITHUB_REPO: 'Nashed',
-    GITHUB_TOKEN: 'ghp_твой-токен-здесь', // Создай токен на GitHub
+    GITHUB_TOKEN: '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', // Создай токен на GitHub
     
     // Путь к аудио файлам в репозитории
     AUDIO_PATH: 'audio'
